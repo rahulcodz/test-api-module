36cb3358d243c32d1601c9f6d0bc5ab7887b106c
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2023-09-19 17:03:14 +0530

    misc

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express"
 import http from "http"
 import cors from "cors"
+import pg from "pg"
 
 const app = express()
 app.use(cors())
+const { Pool } = pg
+const pool = new Pool({
+  connectionString:
+    "postgres://default:6VJSaD9QHwrW@ep-green-pond-38424529-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" +
+    "?sslmode=require",
+})
 
 const server = http.createServer(app)
 
 app.get("/user", async (req, res) => {
-  //   const rows = await sql`SELECT * FROM employees;`
-  res.json("rows")
+  try {
+    const data = await pool.query(`SELECT * FROM employees`)
+    return res.status(200).json(data)
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 })
 
 server.listen(8000, () => {
